09fcc214450d5394ab982d115c083f20f3283ed0
Ryan Ashby <rjapsu160@gmail.com>
2022-01-24 14:01:41 -0800

    test functionality in console. tests pass.
    Co-authored-by: Justin Dechow <justin.dechow@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
-function ToDoList {
+// TO DO LIST OBJECT/METHODS
+
+function ToDoList() {
   this.tasks = {};
   this.taskId = 0;
 }
+
+ToDoList.prototype.addId = function() {
+  this.taskId +=1;
+  return this.taskId;
+}
+
+ToDoList.prototype.addTask = function(task) {
+  task.id = this.addId();
+  this.tasks[task.id] = task;
+}
+
+//  TASK OBJECT/METHODS
 
 function Task (name, date, time, location) {
   this.name = name;
@@ -9,4 +23,10 @@ function Task (name, date, time, location) {
   this.time = time;
   this.location = location;
 }
+
+let myList = new ToDoList;
+let dishes = new Task("dishes", "January 24", "6pm", "kitchen");
+let laundry = new Task("laundry", "January 25", "8pm", "laundry room");
+let groceries = new Task("groceries", "January 27", "4pm", "Fred Meyer");
+
 
